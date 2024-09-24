8e40974b3444041413f08f7adba5879682abc944
cjavatebbin <thwu2021@gmail.com>
2024-09-23 20:35:50 -0700

    Update PepysProblem.java

--- PepysProblem.java
+++ PepysProblem.java
@@ -2,21 +2,6 @@
 /**
  * @author Terence Wu
  * @version 9/23/2024
- * 
- * Sample run 1:
- * 
- *      Welcome to the Pepys Problem.
- *      Enter the number of simulations to run: 1000000
- *      The empirical probability of rolling at least one six if six dice are rolled is: 0.664918
- *      The empirical probability of rolling at least two sixes if 12 dice are rolled is: 0.618532
- *      
- * Sample run 2:
- * 
- *      Welcome to the Pepys Problem.
- *      Enter the number of simulations to run: 10000
- *      The empirical probability of rolling at least one six if six dice are rolled is: 0.6619
- *      The empirical probability of rolling at least two sixes if 12 dice are rolled is: 0.6209
- * 
  */
 
 import java.util.Scanner;
